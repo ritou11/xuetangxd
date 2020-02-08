1f7b1167f768d524705d70577233c6dfa83def4b
haotian <ritou11@gmail.com>
2020-02-08 14:01:42 +0800

    re-wrap

--- index.js
+++ index.js
@@ -47,6 +47,16 @@ module.exports = yargRoot
     describe: 'RSA password of your account.',
     type: 'string',
   })
+  .option('f', {
+    alias: 'cache-file',
+    describe: 'Use specified cache file to start',
+    type: 'string',
+  })
+  .option('o', {
+    alias: 'output-file',
+    describe: 'output cache file to the path',
+    type: 'string',
+  })
   .option('q', {
     alias: 'quality',
     describe: 'High quality or not',
@@ -55,51 +65,15 @@ module.exports = yargRoot
   .command('test <cacheFile>', 'testload the course',
     (yargs) => {
       yargs
-        .positional('cacheFile', {
-          describe: '<cacheFile> The path of cached file',
+        .positional('tttt', {
+          describe: '<tttt> None',
           type: 'string',
         });
     },
     async (argv) => {
-      const config = readConfig(argv);
-      const ck = checkConfig(config);
-      if (ck) {
-        console.error(ck);
-        return;
-      }
-      console.log(`Using ${config.username} ${config.rsaPassword}`);
-      xuetangx.login(config.username, config.rsaPassword).then(async (loginRes) => {
-        const { nickname, school } = loginRes;
-        console.log(`Login as ${nickname}, ${school}.`);
-        const data = JSON.parse(fs.readFileSync(argv.cacheFile));
-        if (!data) {
-          console.log('Failed to get chapters.');
-          return;
-        }
-        const videoLeaves = xuetangx.iterChap(data.course_chapter);
-        for (let i = 0; i < videoLeaves.length; i += 1) {
-          if (!videoLeaves[i].ccid) {
-            console.log(`Getting CCID of ${videoLeaves[i].name}`);
-            // eslint-disable-next-line
-            videoLeaves[i].ccid = await xuetangx.getVideoCcid(videoLeaves[i].id, data.cid, data.sign);
-            console.log(videoLeaves[i].ccid);
-          }
-        }
-        fs.writeFileSync(`outputs/${data.course_id}${data.course_name}.test.json`, JSON.stringify(data, null, 4));
-        for (let i = 0; i < videoLeaves.length; i += 1) {
-          if (!videoLeaves[i].link && videoLeaves[i].ccid) {
-            console.log(`Getting link of ${videoLeaves[i].name}`);
-            // eslint-disable-next-line
-            videoLeaves[i].link = await xuetangx.getPlayurl(videoLeaves[i].ccid, config.quality);
-            console.log(videoLeaves[i].link);
-          }
-        }
-        fs.writeFileSync(`outputs/${data.course_id}${data.course_name}.test.json`, JSON.stringify(data, null, 4));
-      }).catch((e) => {
-        console.log('Login failed.', e);
-      });
+      console.log(argv);
     })
-  .command('down <cid> <sign>', 'download the course',
+  .command('down [<cid>] [<sign>]', 'download the course',
     (yargs) => {
       yargs
         .positional('cid', {
@@ -118,27 +92,57 @@ module.exports = yargRoot
         console.error(ck);
         return;
       }
+      if ((!argv.cid || !argv.sign) && !argv.cacheFile) {
+        console.log('Insufficient input.');
+      }
       console.log(`Using ${config.username} ${config.rsaPassword}`);
       xuetangx.login(config.username, config.rsaPassword).then(async (loginRes) => {
         const { nickname, school } = loginRes;
         console.log(`Login as ${nickname}, ${school}.`);
-        xuetangx.getChapters(argv.cid, argv.sign).then((res) => {
-          const data = res && res.data;
-          if (!data) {
-            console.log('Failed to get chapters.');
-            return;
+        let data;
+        if (argv.cacheFile) {
+          data = JSON.parse(fs.readFileSync(argv.cacheFile));
+        }
+        if (!data && argv.cid && argv.sign) {
+          try {
+            const res = await xuetangx.getChapters(argv.cid, argv.sign);
+            data = res && res.data;
+          } catch (e) {
+            console.log('Failed to get chapters online.');
           }
-          /* eslint-disable */
-          const { course_name, course_id } = data;
-          data.cid = argv.cid;
-          data.sign = argv.sign;
-          fs.writeFileSync(`outputs/${course_id}${course_name}.json`, JSON.stringify(data, null, 4));
-          /* eslint-enable */
-          const videoLeaves = xuetangx.iterChap(data.course_chapter);
-          xuetangx.getVideoLink(videoLeaves[0].leafinfo_id, argv.cid, argv.sign);
-        }).catch((e) => {
-          console.log(e);
-        });
+        }
+        if (!data) {
+          console.log('Failed to get chapters.');
+          return;
+        }
+        data.cid = data.cid || argv.cid;
+        data.sign = data.sign || argv.sign;
+        /* eslint-disable-next-line */
+        const { course_name, course_id } = data;
+        /* eslint-disable-next-line */
+        const output = argv.outputFile || `outputs/${course_id}${course_name}.json`;
+        fs.writeFileSync(output, JSON.stringify(data, null, 4));
+
+        console.log('Select all video leaves...');
+        const videoLeaves = xuetangx.iterChap(data.course_chapter);
+        for (let i = 0; i < videoLeaves.length; i += 1) {
+          if (!videoLeaves[i].ccid) {
+            console.log(`Getting CCID of ${videoLeaves[i].name}`);
+            // eslint-disable-next-line
+            videoLeaves[i].ccid = await xuetangx.getVideoCcid(videoLeaves[i].id, data.cid, data.sign);
+            console.log(videoLeaves[i].ccid);
+          }
+        }
+        fs.writeFileSync(output, JSON.stringify(data, null, 4));
+        for (let i = 0; i < videoLeaves.length; i += 1) {
+          if (!videoLeaves[i].link && videoLeaves[i].ccid) {
+            console.log(`Getting link of ${videoLeaves[i].name}`);
+            // eslint-disable-next-line
+            videoLeaves[i].link = await xuetangx.getPlayurl(videoLeaves[i].ccid, config.quality);
+            console.log(videoLeaves[i].link);
+          }
+        }
+        fs.writeFileSync(output, JSON.stringify(data, null, 4));
       }).catch(() => {
         console.log('Login failed.');
       });
